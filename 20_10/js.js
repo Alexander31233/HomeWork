9bdfecd46f52c4d6b787adc15ceaf37bcaf0fae1
Topolskiy A <sanektop10@gmail.com>
2022-10-20 20:08:40 +0600

    feat: 20_10

---
+++ 20_10/js.js
@@ -0,0 +1,27 @@
+/*
+let arr = [];
+let pushArr = (a , x) => {
+    pushArr(a) =
+// }not done
+ */
+//
+// let array = [1 , 2, 3, ];
+// let revArr = (arr) =>{
+//     return  arr.reverse();
+//
+// }
+// let res = revArr(array);
+// alert(res);
+//
+
+/*
+let a = toNumber(prompt());
+for (let i = 0; i < a.length; i++){
+    if(a[i] > 0){
+        alert("Plus");
+    }else if(a[i] < 0){
+        alert("Minus")
+    }else if (a[i] === 0){
+        break;
+    }
+}*/
